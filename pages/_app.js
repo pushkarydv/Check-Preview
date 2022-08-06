07a59c841e79ca26b4e795be9a1faf6d456952f8
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-08-07 02:59:53 +0530

    minor fixes

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className="transition-all bg-slate-100 min-h-screen  text-slate-800 dark:bg-slate-900 dark:text-slate-50 custom-font">
+    <main className="transition-all bg-slate-100 min-h-screen  text-slate-800 dark:bg-slate-900 dark:text-emerald-50 custom-font">
       <Component {...pageProps} />
     </main>
   );
