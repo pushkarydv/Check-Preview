0cdab83065fb1ae75bea6c5c7aba38075188fcb3
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-08-07 02:45:28 +0530

    added custom fonts and custom scrollbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/tailwind_init.css";
+import "../styles/scrollbar.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="custom-font w-full min-h-screen overflow-x-hidden bg-slate-100 text-slate-700">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
