39cb637b65c07bc384e16c4bf5384baebfd87a70
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-08-07 11:31:57 +0530

    basic code splitting

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import "../styles/tailwind_init.css";
 import "../styles/scrollbar.css";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className="transition-all bg-slate-100 min-h-screen  text-slate-800 dark:bg-slate-900 dark:text-emerald-50 custom-font">
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        {" "}
+        <title>Check Preview</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <main className="transition-all bg-slate-100 min-h-screen  text-slate-800 dark:bg-slate-900 dark:text-emerald-50 custom-font">
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -21,47 +21,6 @@ export default function Home() {
   }
   return (
     <>
-      <Head>
-        <title>Check Preview</title>
-        <meta name="title" content="Card Preview" />{" "}
-        <meta
-          name="description"
-          content="A Small website to check how Cards of your website looks when it get's shared via email or any social media"
-        />{" "}
-        <meta
-          name="keywords"
-          content="card, metadata, mets, tags, banner, web cards, Check Preview"
-        />{" "}
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />{" "}
-        <meta name="language" content="English" />{" "}
-        <meta name="author" content="Pushkar Yadav" />{" "}
-        <meta property="og:type" content="website" />{" "}
-        <meta property="og:url" content="https://checkpreview.vercel.app" />{" "}
-        <meta property="og:title" content="Card Preview" />{" "}
-        <meta
-          property="og:description"
-          content="A Small website to check how Cards of your website looks when it get's shared via email or any social media"
-        />{" "}
-        <meta
-          property="og:image"
-          content="https://checkpreview.vercel.app/banner.png"
-        />{" "}
-        <meta property="twitter:card" content="summary_large_image" />{" "}
-        <meta
-          property="twitter:url"
-          content="https://checkpreview.vercel.app"
-        />{" "}
-        <meta property="twitter:title" content="Card Preview" />{" "}
-        <meta
-          property="twitter:description"
-          content="A Small website to check how Cards of your website looks when it get's shared via email or any social media"
-        />
-        <meta
-          property="twitter:image"
-          content="https://checkpreview.vercel.app/banner.png"
-        />
-        <link rel="icon" href="/logo.png" />
-      </Head>
       <Header />
       <div className="flex flex-col md:flex-row p-4 my-4">
         <div className="w-full md:w-[60%] py-4">
